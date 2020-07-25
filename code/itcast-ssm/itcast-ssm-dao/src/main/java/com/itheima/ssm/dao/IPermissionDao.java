b0e8340ad6722688a3766fdada4510858bfa0134
Rz <243566431@qq.com>
2020-07-25 18:17:59 +0800

    permission manage

--- code/itcast-ssm/itcast-ssm-dao/src/main/java/com/itheima/ssm/dao/IPermissionDao.java
+++ code/itcast-ssm/itcast-ssm-dao/src/main/java/com/itheima/ssm/dao/IPermissionDao.java
@@ -1,11 +1,19 @@
 package com.itheima.ssm.dao;
 
 import com.itheima.ssm.domain.Permission;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 
+import java.rmi.server.ExportException;
 import java.util.List;
 
 public interface IPermissionDao {
     @Select("select * from permission where id in (select permissionid from role_permission where roleid = #{id})")
     List<Permission> findPermissionByRoleId(String id) throws Exception;
+
+    @Select("select * from permission")
+    List<Permission> findAll() throws Exception;
+
+    @Insert("insert into permission(permissionName,url) values(#{permissionName},#{url})")
+    public void save(Permission permission) throws Exception;
 }
