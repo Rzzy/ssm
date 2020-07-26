9720f025f5faad82644914f136c4bdcb7ac02935
Rz <243566431@qq.com>
2020-07-27 00:56:13 +0800

    update

---
+++ code/itcast-ssm/itcast-ssm-service/src/main/java/com/itheima/ssm/service/impl/SysLogServiceImpl.java
@@ -0,0 +1,23 @@
+package com.itheima.ssm.service.impl;
+
+import com.itheima.ssm.dao.ISysLogDao;
+import com.itheima.ssm.domain.SysLog;
+import com.itheima.ssm.service.ISysLogService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class SysLogServiceImpl implements ISysLogService {
+    @Autowired
+    private ISysLogDao sysLogDao;
+
+    @Override
+    public List<SysLog> findAll() throws Exception {
+        return sysLogDao.findAll();
+    }
+
+    @Override
+    public void save(SysLog sysLog) throws Exception {
+        sysLogDao.save(sysLog);
+    }
+}
